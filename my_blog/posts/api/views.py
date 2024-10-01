454bcf6d4c991bbca3cca15c98a71b157390faee
Marcos Magaña <imarcosmagana@gmail.com>
2024-09-30 18:14:21 -0600

    eliminando lineas comentadas

--- my_blog/posts/api/views.py
+++ my_blog/posts/api/views.py
@@ -7,17 +7,10 @@ from posts.api.serializers import PostSerializer
 
 class PostApiView(APIView):
     def get(self, request):
-        #posts = Post.objects.all()
-        # posts = [post.title for post in Post.objects.all()]
-        # return Response(status=status.HTTP_200_OK,data=posts)
         serializer = PostSerializer(Post.objects.all(),many=True)
         return Response(status=status.HTTP_200_OK,data=serializer.data)
     
     def post(self, request):
-        #Post.objects.create(title=request.POST['title'], 
-        #                    description=request.POST['description'], 
-        #                    order=request.POST['order'])
-        #  return self.get(request)
         serializer = PostSerializer(data=request.POST)
         serializer.is_valid(raise_exception=True)
         serializer.save()
