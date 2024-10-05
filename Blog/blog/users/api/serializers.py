dafbcdd49af93cb5057e791ce9731422dac3b60a
Marcos Magaña <imarcosmagana@gmail.com>
2024-10-04 21:53:58 -0600

    encriptando contraseña en el registro de usuario

--- Blog/blog/users/api/serializers.py
+++ Blog/blog/users/api/serializers.py
@@ -6,4 +6,10 @@ class UserRegisterSerializer(serializers.ModelSerializer):
         model = User
         fields = ['id','email','username','password']
 
-    
+    def create(self, validated_data):
+        password = validated_data.pop('password',None)
+        instance = self.Meta.model(**validated_data)
+        if password is not None:
+            instance.set_password(password)
+        instance.save()
+        return instance
