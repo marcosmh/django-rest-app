68bd20610003d7e24a86739b31a2701e76f68a0b
Marcos Magaña <imarcosmagana@gmail.com>
2024-10-04 21:53:17 -0600

    Registro de Usuario

--- Blog/blog/blog/urls.py
+++ Blog/blog/blog/urls.py
@@ -14,9 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+
 
 schema_view = get_schema_view(
    openapi.Info(
@@ -35,4 +36,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redocs/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('api/', include('users.api.router')),
 ]
