a7af4ad0f79b5a62d52cdc299ff9bcd99bbdfbf2
Marcos Magaña <imarcosmagana@gmail.com>
2024-10-09 20:05:40 -0600

    actualizando datos del usuario

--- Blog/blog/users/api/views.py
+++ Blog/blog/users/api/views.py
@@ -3,7 +3,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from users.models import User
-from users.api.serializers import UserRegisterSerializer, UserSerializer
+from users.api.serializers import UserRegisterSerializer, UserSerializer, UserUpdateSerializer
 
 class RegisterView(APIView):
     def post(self, request):
@@ -20,5 +20,17 @@ class UserView(APIView):
     permission_classes = [IsAuthenticated]
 
     def get(self, request):
+        print('Obtener usuario....')
         serializer = UserSerializer(request.user)
         return Response(serializer.data)
+    
+    def put(self, request):
+        print('Actualizando usuario....')
+        user = User.objects.get(id=request.user.id)
+        serializer = UserUpdateSerializer(user, request.data)
+
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data)
+        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
