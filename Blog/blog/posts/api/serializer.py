dc2037d1bbb6d17566031d548174708d43b4d5a8
Marcos Magaña <imarcosmagana@gmail.com>
2024-10-15 11:47:40 -0600

    modificacion serializer

--- Blog/blog/posts/api/serializer.py
+++ Blog/blog/posts/api/serializer.py
@@ -6,7 +6,6 @@ from categories.api.serializers import CategorySerializer
 class PostSerializer(serializers.ModelSerializer):
     user = UserSerializer()
     category = CategorySerializer()
-
     class Meta:
         model = Post
         fields = ['title','content','slug','miniature','created_at','published','user','category']
